28c22e3050ee5b31dbf648f76a853cf8193699fd
JovRiver <59974497+JovRiver@users.noreply.github.com>
2020-01-24 16:37:51 -0500

    New Objects
    A box object with collision and a mildly functional ramp.

--- game/main.js
+++ game/main.js
@@ -111,136 +111,134 @@ function setupGraphics(){
 
 function createObstacles() {
 
-	//ledge variables
-	var pos = {x: 25, y: 10, z: 0};
-	var pos2 = {x: 11, y: 3, z: 0};
-	var pos3 = {x: -5, y: 10, z: 0};
+	//object position variables
+	var ledge1Pos = {x: 25, y: 10, z: 0};
+	var ledge2Pos = {x: -5, y: 10, z: 0};
+    var rampPos = {x: 25, y: 2.65, z: 16.2};
+	var box1Pos = {x: 11, y: 3, z: 0};
 
-	var scale = {x: 20, y: 1, z: 20};
-	var scale2 = {x: 5, y: 5, z: 5};
+    //object scale variables
+	var ledgeScale = {x: 20, y: 1, z: 20};
+	var rampScale = {x: 20, y: 1, z: 15};
+    var boxScale = {x: 5, y: 5, z: 5};
 
+    //object quaternion variables
 	var quat = {x: 0, y: 0, z: 0, w: 1};
-	var r1quat = {x: 0, y: 45, z: 0, w: 1};
+	var rampQuat = {x: Math.cos(45), y: 0, z: 0, w: 1};
+    
+    //object mass
 	var mass = 0;
-
-	//ramp variables
-	var r1Pos = {x: 25, y: 7, z: 14.58};
-	var r2Pos = {x: 25, y: 5.2, z: -15.4};
-	var rScale = {x: 20, y: 1, z: 15};
 
 //three.js section
 
-	//ledge creation
-	var ledge = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
-	ledge.position.set(pos.x, pos.y, pos.z);
-	ledge.scale.set(scale.x, scale.y, scale.z);
-	ledge.castShadow = true;
-	ledge.receiveShadow = true;
-	scene.add(ledge);
+	//ledge1 creation
+	var ledge1 = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
+        ledge1.position.set(ledge1Pos.x, ledge1Pos.y, ledge1Pos.z);
+        ledge1.scale.set(ledgeScale.x, ledgeScale.y, ledgeScale.z);
+        ledge1.castShadow = true;
+        ledge1.receiveShadow = true;
+
+    scene.add(ledge1);
 
 	//ledge2 creation
 	var ledge2 = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
-	ledge2.position.set(pos3.x, pos3.y, pos3.z);
-	ledge2.scale.set(scale.x, scale.y, scale.z);
-	ledge2.castShadow = true;
-	ledge2.receiveShadow = true;
-	scene.add(ledge2);
+        ledge2.position.set(ledge2Pos.x, ledge2Pos.y, ledge2Pos.z);
+        ledge2.scale.set(ledgeScale.x, ledgeScale.y, ledgeScale.z);
+        ledge2.castShadow = true;
+        ledge2.receiveShadow = true;
+    
+    scene.add(ledge2);
 
-	//stepUpLedge creation
-	var stepUpLedge = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
-	stepUpLedge.position.set(pos2.x, pos2.y, pos2.z);
-	stepUpLedge.scale.set(scale2.x, scale2.y, scale2.z);
-	stepUpLedge.castShadow = true;
-	stepUpLedge.receiveShadow = true;
-	scene.add(stepUpLedge);
+	//box1Test creation
+	var box1Test = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
+        box1Test.position.set(box1Pos.x, box1Pos.y, box1Pos.z);
+        box1Test.scale.set(boxScale.x, boxScale.y, boxScale.z);
+        box1Test.castShadow = true;
+        box1Test.receiveShadow = true;
+    
+    scene.add(box1Test);
 
-	//ramp1 creation
-	var ramp1 = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 , wireframe: true}));
-	ramp1.position.set(r1Pos.x, r1Pos.y, r1Pos.z);
-	ramp1.scale.set(rScale.x, rScale.y, rScale.z);
-	ramp1.castShadow = true;
-	ramp1.receiveShadow = true;
-	ramp1.rotation.x = Math.sin(45);
-	scene.add(ramp1);
-
-	//ramp2 creation
-	var ramp2 = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
-	ramp2.position.set(r2Pos.x, r2Pos.y, r2Pos.z);
-	ramp2.scale.set(rScale.x, rScale.y, rScale.z);
-	ramp2.castShadow = true;
-	ramp2.receiveShadow = true;
-	ramp2.rotation.x = Math.cos( 3 * Math.PI / 4);
-	scene.add(ramp2);
+	//ramp creation
+	var ramp = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xa0afa4 }));
+        ramp.position.set(rampPos.x, rampPos.y, rampPos.z);
+        ramp.scale.set(ledgeScale.x, ledgeScale.y, ledgeScale.z);
+        ramp.castShadow = true;
+        ramp.receiveShadow = true;
+        ramp.rotation.x = Math.sin(45);
+    
+    scene.add(ramp);
 
 //ammo.js section
 
-	//ledge transform
-	var transform = new Ammo.btTransform();
-	transform.setIdentity();
-	transform.setOrigin( new Ammo.btVector3( pos.x, pos.y, pos.z ) );
-	transform.setRotation( new Ammo.btQuaternion( quat.x, quat.y, quat.z, quat.w ) );
-	var motionState = new Ammo.btDefaultMotionState( transform );
-	var colShape = new Ammo.btBoxShape( new Ammo.btVector3( scale.x * 0.5, scale.y * 0.5, scale.z * 0.5 ) );
-	colShape.setMargin( 0.05 );
-	var localInertia = new Ammo.btVector3( 0, 0, 0 );
-	colShape.calculateLocalInertia( mass, localInertia );
-	var rbInfo = new Ammo.btRigidBodyConstructionInfo( mass, motionState, colShape, localInertia );
-	var body = new Ammo.btRigidBody( rbInfo );
-	body.setFriction(4);
-	body.setRollingFriction(10);
-	physicsWorld.addRigidBody( body );
+	//ledge1 transform
+	var ledge1Transform = new Ammo.btTransform();
+        ledge1Transform.setIdentity();
+        ledge1Transform.setOrigin( new Ammo.btVector3( ledge1Pos.x, ledge1Pos.y, ledge1Pos.z ) );
+        ledge1Transform.setRotation( new Ammo.btQuaternion( quat.x, quat.y, quat.z, quat.w ) );
+	var ledge1MotionState = new Ammo.btDefaultMotionState( ledge1Transform );
+	var ledge1ColShape = new Ammo.btBoxShape( new Ammo.btVector3( ledgeScale.x * 0.5, ledgeScale.y * 0.5, ledgeScale.z * 0.5 ) );
+        ledge1ColShape.setMargin( 0.05 );
+	var ledge1LocalInertia = new Ammo.btVector3( 0, 0, 0 );
+        ledge1ColShape.calculateLocalInertia( mass, ledge1LocalInertia );
+	var ledge1RBInfo = new Ammo.btRigidBodyConstructionInfo( mass, ledge1MotionState, ledge1ColShape, ledge1LocalInertia );
+	var ledge1Body = new Ammo.btRigidBody( ledge1RBInfo );
+        ledge1Body.setFriction(4);
+        ledge1Body.setRollingFriction(10);
+	
+    physicsWorld.addRigidBody( ledge1Body );
 
-	//ramp1 transform
-	var transform1 = new Ammo.btTransform();
-	transform1.setIdentity();
-	transform1.setOrigin( new Ammo.btVector3( r1Pos.x, r1Pos.y, r1Pos.z ) );
-	transform1.setRotation( new Ammo.btQuaternion( r1quat.x, r1quat.y, r1quat.z, r1quat.w ) );
-	var motionState1 = new Ammo.btDefaultMotionState( transform1 );
-	var colShape1 = new Ammo.btBoxShape( new Ammo.btVector3( rScale.x *.5 , rScale.y *.5 , rScale.z *.5  ) );
-	colShape1.setMargin( 0.05 );
-	var localInertia1 = new Ammo.btVector3( 0, 0, 0 );
-	colShape1.calculateLocalInertia( mass, localInertia1 );
-	var rbInfo1 = new Ammo.btRigidBodyConstructionInfo( mass, motionState1, colShape1, localInertia1 );
-	var body1 = new Ammo.btRigidBody( rbInfo1 );
-	body1.setFriction(4);
-	body1.setRollingFriction(10);
-	physicsWorld.addRigidBody( body1 );
+	//ledge2 transform
+	var ledge2Transform = new Ammo.btTransform();
+        ledge2Transform.setIdentity();
+        ledge2Transform.setOrigin( new Ammo.btVector3( ledge2Pos.x, ledge2Pos.y, ledge2Pos.z ) );
+        ledge2Transform.setRotation( new Ammo.btQuaternion( quat.x, quat.y, quat.z, quat.w ) );
+	var ledge2MotionState = new Ammo.btDefaultMotionState( ledge2Transform );
+	var ledge2ColShape = new Ammo.btBoxShape( new Ammo.btVector3( ledgeScale.x * 0.5, ledgeScale.y * 0.5, ledgeScale.z * 0.5 ) );
+        ledge2ColShape.setMargin( 0.05 );
+	var ledge2LocalInertia = new Ammo.btVector3( 0, 0, 0 );
+        ledge2ColShape.calculateLocalInertia( mass, ledge2LocalInertia );
+	var ledge2RBInfo = new Ammo.btRigidBodyConstructionInfo( mass, ledge2MotionState, ledge2ColShape, ledge2LocalInertia );
+	var ledge2Body = new Ammo.btRigidBody( ledge2RBInfo );
+        ledge2Body.setFriction(4);
+        ledge2Body.setRollingFriction(10);
+	
+    physicsWorld.addRigidBody( ledge2Body );
 
-	//ramp2 transform
-	var transform2 = new Ammo.btTransform();
-	transform2.setIdentity();
-	transform2.setOrigin( new Ammo.btVector3( r2Pos.x, r2Pos.y, r2Pos.z ) );
-	transform2.setRotation( new Ammo.btQuaternion( quat.x, quat.y, quat.z, quat.w ) );
-	var motionState2 = new Ammo.btDefaultMotionState( transform2 );
-	var colShape2 = new Ammo.btBoxShape( new Ammo.btVector3( rScale.x * 0.5, rScale.y * 0.5, rScale.z * 0.5 ) );
-	colShape2.setMargin( 0.05 );
-	var localInertia2 = new Ammo.btVector3( 0, 0, 0 );
-	colShape2.calculateLocalInertia( mass, localInertia2 );
-	var rbInfo2 = new Ammo.btRigidBodyConstructionInfo( mass, motionState2, colShape2, localInertia2 );
-	var body2 = new Ammo.btRigidBody( rbInfo2 );
-	body2.setFriction(4);
-	body2.setRollingFriction(10);
-	physicsWorld.addRigidBody( body2 );
+	//box1Test transform
+	var box1Transform = new Ammo.btTransform();
+        box1Transform.setIdentity();
+        box1Transform.setOrigin( new Ammo.btVector3( box1Pos.x, box1Pos.y, box1Pos.z ) );
+        box1Transform.setRotation( new Ammo.btQuaternion( quat.x, quat.y, quat.z, quat.w ) );
+	var box1MotionState = new Ammo.btDefaultMotionState( box1Transform );
+	var box1ColShape = new Ammo.btBoxShape( new Ammo.btVector3( boxScale.x * 0.5, boxScale.y * 0.5, boxScale.z * 0.5 ) );
+        box1ColShape.setMargin( 0.05 );
+	var box1LocalInertia = new Ammo.btVector3( 0, 0, 0 );
+        box1ColShape.calculateLocalInertia( mass, box1LocalInertia );
+	var box1RBInfo = new Ammo.btRigidBodyConstructionInfo( mass, box1MotionState, box1ColShape, box1LocalInertia );
+	var box1Body = new Ammo.btRigidBody( box1RBInfo );
+        box1Body.setFriction(4);
+        box1Body.setRollingFriction(10);
+	
+    physicsWorld.addRigidBody( box1Body );
 
-	//stepUpLedge transform
-	//TODO
-
-	//ledge2 creation
-	//TODO
+	//ramp transform
+	var rampTransform = new Ammo.btTransform();
+        rampTransform.setIdentity();
+        rampTransform.setOrigin( new Ammo.btVector3( rampPos.x, rampPos.y + 1, rampPos.z - 1 ) );
+        rampTransform.setRotation( new Ammo.btQuaternion( rampQuat.x, rampQuat.y, rampQuat.z, rampQuat.w ) );
+	var rampMotionState = new Ammo.btDefaultMotionState( rampTransform );
+	var rampColShape = new Ammo.btBoxShape( new Ammo.btVector3( rampScale.x * 0.5, rampScale.y * 0.5, rampScale.z * 0.5 ) );
+        rampColShape.setMargin( 0.05 );
+	var rampLocalInertia = new Ammo.btVector3( 0, 0, 0 );
+        rampColShape.calculateLocalInertia( mass, rampLocalInertia );
+	var rampRBInfo = new Ammo.btRigidBodyConstructionInfo( mass, rampMotionState, rampColShape, rampLocalInertia );
+	var rampBody = new Ammo.btRigidBody( rampRBInfo );
+        rampBody.setFriction(1);
+        rampBody.setRollingFriction(10);
+	
+    physicsWorld.addRigidBody( rampBody );
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
